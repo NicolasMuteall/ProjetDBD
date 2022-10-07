13bc76027bb41a0399fefbadb6c3dbd88ad10d99
NicolasDpnt <nicolas.dupont59176@gmail.com>
2022-10-07 16:12:07 +0200

    UPDATED DATA.

--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,8 @@
-<?php include('head.php'); ?>
+<?php 
+        session_start();
+        $_SESSION['role'] = 'visit';
+        include('head.php'); 
+?>
 <header class="container-fluid">
         <img class="shadow-lg" id="banniere" src="../images/BanniereDBD.jpg" alt="BanniereDBD">
 </header>
