53b9c375c17f165cdacf66f499a353bc14179c3f
NicolasDpnt <nicolas.dupont59176@gmail.com>
2022-10-07 11:45:22 +0200

    ADD footer.

---
+++ PHP/accueil.php
@@ -0,0 +1,5 @@
+<?php include('head.php'); ?>
+<header class="container-fluid">
+        <img class="shadow-lg" id="banniere" src="../images/BanniereDBD.jpg" alt="BanniereDBD">
+</header>
+<?php include('footer.php'); ?>
---
+++ PHP/head.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" 
+    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <title>Dead by daylight</title>
+    <link rel="icon" type="image/png" href="../images/logonoir.png" />
+</head>
+<body class="bg-dark text-white">
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top" style="background-color: #001219;">
+        <div class="container-fluid">
+            <a id="logo" class="navbar-brand" href="#">
+                <img src="../images/logo.png" alt="logodbd" width="60" height="54">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul id="listnav" class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Personnages</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Joueurs</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Se connecter</a>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Recherche..." aria-label="Search">
+                <button class="btn btn-outline-secondary" type="submit">Rechercher</button>
+              </form>
+            </div>
+        </div>
+    </nav>
+    <main>
+
+    
