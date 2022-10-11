ed08e97e831a628d5bf1b741ce0dae6580a59edc
NicolasDpnt <nicolas.dupont59176@gmail.com>
2022-10-11 12:06:37 +0200

    ADD img.

--- PHP/personnage.php
+++ PHP/personnage.php
@@ -3,25 +3,36 @@
     if(empty($_SESSION)){
         $_SESSION['role']="";
     }
-    include('head.php');
-    include('Connect.php');
 ?>
 
 <?php 
-    var_dump($_SESSION['role']); 
+     
     if($_SESSION['role'] === ""){
 
         header('Location: role.php');
 
-    }else{ ?>
+    }else{ 
+
         
+        include('head.php');
+        var_dump($_SESSION['role'],$_GET['role']);
+        include('Connect.php');
+
+        $perso = $cnx->prepare('select * from personnage where NOM_ROLE="'.$_GET['role'].'"');
+        $perso -> bindvalue('NOM_ROLE', $_GET['role']);
+        $perso -> execute();
+        $resultperso = $perso->fetchall(PDO::FETCH_OBJ);
+
+        $cnx = null;
+        ?>
+
         <div id="personnages" class="container-fluid">
             <div class="row ligneperso">
-                <div class="border boitetueur mb-5 rounded shadow-lg"></div>
-                <div class="border boitetueur mb-5 rounded shadow-lg"></div>
-                <div class="border boitetueur mb-5 rounded shadow-lg"></div>
-                <div class="border boitetueur mb-5 rounded shadow-lg"></div>
-                <div class="border boitetueur mb-5 rounded shadow-lg"></div>
+                <?php foreach($resultperso as $resultat){ ?>
+                <div class="border boitetueur text-center mb-5 rounded shadow-lg">
+                    <h4><?= $resultat->NOM_PERSO ?></h4>
+                    <img class="imgperso img-fluid " src="../images/<?= $_GET['role'] ?>/<?= $resultat->REF_IMAGE_PERSO ?>" alt="<?= $resultat->NOM_PERSO ?>"> 
+                </div> <?php } ?>
             </div>
         </div>
     <?php } ?>
